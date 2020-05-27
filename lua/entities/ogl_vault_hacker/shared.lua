a9586a6bd3b6d816ec8edf6248fe7ab90cde1610
Tom <tom@tomdotbat.dev>
2020-05-27 19:13:40 +0100

    linter makes me kms

--- lua/entities/ogl_vault_hacker/shared.lua
+++ lua/entities/ogl_vault_hacker/shared.lua
@@ -1,7 +1,15 @@
 
 ENT.Type = "anim"
-ENT.Base = "base_entity"
+ENT.Base = "base_gmodentity"
 ENT.PrintName = "OGL Security Hacking Device"
 ENT.Category = "OGL Vault"
 ENT.Author = "Tom.bat"
 ENT.Spawnable = true
+
+function ENT:SetupDataTables()
+    self:NetworkVar("Bool", 0, "Opened")
+
+    if SERVER then
+        self:SetOpened(false)
+    end
+end
