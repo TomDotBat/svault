bc4a38a92b23ddbf4894f0cb449a2f40b19c3042
Tom <tom@tomdotbat.dev>
2020-05-27 20:45:42 +0100

    Vault hacker draw method

--- lua/entities/ogl_vault_hacker/cl_init.lua
+++ lua/entities/ogl_vault_hacker/cl_init.lua
@@ -1,2 +1,17 @@
 
 include("shared.lua")
+
+function ENT:Initialize()
+    self.LocalPlayer = LocalPlayer()
+end
+
+function ENT:Think()
+    self.ShouldDraw3D2D = self.LocalPlayer:GetPos():DistToSqr(self:GetPos()) < ogl_vault.config.draw3d2ddist
+end
+
+function ENT:Draw()
+    self:DrawModel()
+
+    if not ogl_vault.lang then return end
+    if not self.ShouldDraw3D2D then return end
+end
