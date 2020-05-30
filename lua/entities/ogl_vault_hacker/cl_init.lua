441c8b4b6527962dc0e270506be28cda25a55509
Tom <tom@tomdotbat.dev>
2020-05-30 13:08:49 +0100

    Position screen 3d2d

--- lua/entities/ogl_vault_hacker/cl_init.lua
+++ lua/entities/ogl_vault_hacker/cl_init.lua
@@ -9,17 +9,28 @@ function ENT:Initialize()
     self.LocalPlayer = LocalPlayer()
 end
 
+local scrw, scrh = 630, 497
 function ENT:DrawTranslucent()
     self:DrawModel()
 
     if not ogl_vault.lang then return end
 
-    --Use the flippy out screen bone pos/angs?
-    --Or wait for anim to play then animate screen turn on
+    local pos, ang = self:GetBonePosition(1)
 
-    if imgui.Entity3D2D(self, Vector(0, 0, 50), Angle(0, 90, 90), 0.06, ogl_vault.config.draw3d2ddist, ogl_vault.config.draw3d2ddist - 40) then
+    ang = self:WorldToLocalAngles(ang)
+
+    pos = self:WorldToLocal(pos) + ang:Up() * -22.42
+    pos = pos + ang:Forward() * 1.18
+    pos = pos + ang:Right() * 5.74
+
+    ang:RotateAroundAxis(ang:Right(), 90)
+    ang:RotateAroundAxis(ang:Up(), 90)
+
+    if imgui.Entity3D2D(self, pos, ang, 0.04, ogl_vault.config.draw3d2ddist, ogl_vault.config.draw3d2ddist - 40) then
         surface.SetDrawColor(color_white)
-        surface.DrawRect(0, 0, 100, 100)
+        surface.DrawRect(0, 0, scrw, scrh)
+
+        draw.SimpleText("poop", "DermaDefault", scrw / 2, scrh / 2, color_black)
 
         imgui.End3D2D()
     end
