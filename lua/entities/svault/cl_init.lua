8d96faae8d819d1d7b20259b6f08708237c68df1
Tom <tom@tomdotbat.dev>
2020-06-20 15:49:08 +0100

    User friendliness

--- lua/entities/svault/cl_init.lua
+++ lua/entities/svault/cl_init.lua
@@ -93,7 +93,7 @@ function ENT:Initialize()
 end
 
 function ENT:Think()
-    self.ShouldDraw3D2D = self.LocalPlayer:GetPos():DistToSqr(self:GetPos()) < svault.config.draw3d2ddist * svault.config.draw3d2ddist
+    self.ShouldDraw3D2D = self.LocalPlayer:GetPos():DistToSqr(self:GetPos()) < svault.config.draw3d2ddist ^ 2
     local robbers = string.Explode("\n", self:GetRobberNames())
     table.Empty(self.Robbers)
 
