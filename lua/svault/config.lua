83f3365d401b007d9f2250f0f52fa6acc55295b1
Tom <tom@tomdotbat.dev>
2020-06-20 12:05:55 +0100

    Hacker control colors

--- lua/svault/config.lua
+++ lua/svault/config.lua
@@ -93,6 +93,10 @@ svault.config.hackerLetterCorrectCol = Color(255, 0, 0)
 svault.config.hackerFinishTextCol = Color(255, 255, 255)
 svault.config.hackerFinishStandBackCol = Color(255, 0, 0)
 
+svault.config.hackerControlBgCol = Color(66, 61, 54, 220)
+svault.config.hackerButtonCol = Color(255, 255, 255)
+svault.config.hackerButtonHoverCol = Color(200, 200, 200)
+
 --[[-------------------------------------------------------------------------
 End of sVault Configuration
 ---------------------------------------------------------------------------]]
