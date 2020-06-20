f0073fb4ee56aacf39837bfbb0807490c9b6076f
Tom <tom@tomdotbat.dev>
2020-06-20 15:50:39 +0100

    Hack completion + targeted vault networking

--- lua/entities/svault_hacker/init.lua
+++ lua/entities/svault_hacker/init.lua
@@ -42,6 +42,9 @@ end
 
 function ENT:SelfDestruct(skipTimer)
     if not skipTimer then
+        self:SetScreenID(3)
+        self:SetSelfDestructTime(CurTime() + svault.config.hackerselfdestructtime)
+
         timer.Create("sVault.HackerSelfDestruct:" .. self:EntIndex(), svault.config.hackerselfdestructtime, 1, function()
             if not IsValid(self) then return end
             self:SelfDestruct(true)
@@ -85,21 +88,28 @@ end
 net.Receive("sVaultHackerPressStart", function(len, ply)
     local ent = net.ReadEntity()
     if ent:GetClass() != "svault_hacker" then return end
+    if ent:GetHacker() != ply then return end
     if ent:GetPos():DistToSqr(ply:GetPos()) > 10000 then return end
     if ent:GetScreenID() != 1 then return end
-    if ent:GetHacker() != ply then return end
 
-    ent:SetScreenID(2)
+    local targetVault = net.ReadEntity()
+    if targetVault:GetClass() != "svault" then return end
+    if ent:GetPos():DistToSqr(targetVault:GetPos()) > svault.config.hackernearbyvaultdist ^ 2 then return end
+    if targetVault:GetState() != VAULT_IDLE then return end
+
     ent.TargetWord = sendHackWord(ent)
     ent.HackStart = CurTime()
+    ent.TargetVault = targetVault
+
+    ent:SetScreenID(2)
 end)
 
 net.Receive("sVaultHackerPressClose", function(len, ply)
     local ent = net.ReadEntity()
     if ent:GetClass() != "svault_hacker" then return end
+    if ent:GetHacker() != ply then return end
     if ent:GetPos():DistToSqr(ply:GetPos()) > 10000 then return end
     if ent:GetScreenID() != 1 then return end
-    if ent:GetHacker() != ply then return end
 
     ent:ResetSequence(0)
     timer.Simple(1, function() --Turn off the screen once fully closed
@@ -108,6 +118,24 @@ net.Receive("sVaultHackerPressClose", function(len, ply)
     end)
 end)
 
+
+net.Receive("sVaultHackerCompleteHack", function(len, ply)
+    local ent = net.ReadEntity()
+    if ent:GetClass() != "svault_hacker" then return end
+    if ent:GetHacker() != ply then return end
+    if ent:GetPos():DistToSqr(ply:GetPos()) > 10000 then return end
+    if ent:GetScreenID() != 2 then return end
+    if not ent.TargetWord then return end
+    if not ent.HackStart then return end
+    if not (ent.TargetVault and IsValid(ent.TargetVault)) then return end
+    if ent.HackStart + svault.config.hackerminhacktime > CurTime() then return end
+    if ent.TargetWord != net.ReadString() then return end
+
+    ent:SelfDestruct()
+    ent.TargetVault:HackVault()
+end)
+
 util.AddNetworkString("sVaultHackerPressStart")
 util.AddNetworkString("sVaultHackerPressClose")
 util.AddNetworkString("sVaultHackerSendTargetWord")
+util.AddNetworkString("sVaultHackerCompleteHack")
