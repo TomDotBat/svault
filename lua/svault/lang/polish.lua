31b78b97bb4251f59cd935b0aedfdfdc6d4bcbfb
Tom <tom@tomdotbat.dev>
2020-07-22 17:35:08 +0100

    Polish translation

---
+++ lua/svault/lang/polish.lua
@@ -0,0 +1,76 @@
+local lang = {}
+
+lang.securitysystem = "SYSTEM BEZPIECZEŃSTWA"
+lang.enabled = "WŁĄCZONE"
+lang.disabled = "NIEPEŁNOSPRAWNY"
+lang.alertcps = "WYKRYTO RUCH\nALERTING CAŁEGO EGZEKWOWANIA PRAWA\nW:"
+lang.securityenabled = "CZUJNIKI RUCHU: WŁĄCZONE\nKAMERY BEZPIECZEŃSTWA: NA ŻYWO\nSYSTEM BEZPIECZEŃSTWA FUNKCJONALNY"
+lang.securitydisabledmsg = "OSTRZEŻENIE!\nSYSTEM BEZPIECZEŃSTWA WYŁĄCZONY\nPROSZĘ PRZEPROWADŹ ŚLEDZTWO"
+lang.robberystatus = "ROZBÓJ STATUS"
+lang.warmupperiod = "OKRES ROZGRZEWKI"
+lang.cooldownperiod = "OKRES ODNOWIENIA"
+lang.vaultstatus = "STAN RÓŻNY"
+lang.idle = "BEZCZYNNY"
+lang.recovering = "ODZYSKIWANIE"
+lang.info = "INFO"
+lang.cooldown = "OCHŁONĄĆ"
+lang.active = "AKTYWNY"
+lang.inactive = "NIEAKTYWNY"
+lang.players = "GRACZE"
+lang.totalvalue = "ŁĄCZNA WARTOŚĆ"
+lang.lawenforcement = "EGZEKWOWANIE PRAWA"
+lang.currentrobbers = "AKTUALNI ZŁOTA"
+lang.notapplicable = "N/A"
+lang.savevaultscmd = "/savevaults"
+lang.clearvaultscmd = "/clearvaults"
+lang.raidsuccess = "Nalot się udał, zostałeś nagrodzony pieniędzmi ze skarbca bankowego."
+lang.nopermission = "Nie masz uprawnień, aby edytować lokalizacje skarbców."
+lang.savedvaults = "Zapisano lokalizacje wszystkich skarbców na mapie."
+lang.removedvaults = "Niezapisane i usunięte wszystkie skarbce."
+lang.cantstartcooldown = "Nie możesz rozpocząć rajdu w czasie odnowienia."
+lang.wrongteam = "Nie możesz napadać jako twoja praca."
+lang.toomanyraids = "Trwa zbyt wiele nalotów, aby rozpocząć nowy."
+lang.notenoughplayers = "Nie ma wystarczającej liczby graczy, aby rozpocząć napad"
+lang.notenoughpolice = "W sieci nie ma wystarczającej liczby organów ścigania, aby rozpocząć nalot."
+lang.alreadytakingpart = "Już bierzesz udział w trwającym nalocie."
+lang.alreadybeingraided = "Ten skarbiec już jest atakowany."
+lang.toomanyraiders = "Nie możesz dołączyć do drużyny rajdowej dla tego skarbca, ponieważ jest on pełny."
+lang.toolate = "Nie możesz już dołączyć do rajdu, ponieważ jest już za późno."
+lang.cantchangeteam = "Nie możesz zmienić pracy podczas nalotu."
+lang.cantsleep = "Nie możesz spać podczas nalotu."
+lang.cantstartvote = "Nie możesz rozpocząć głosowania podczas nalotu."
+lang.killedraiderreward = "Zostałeś nagrodzony %m za zabicie najeźdźca."
+lang.friendlyfirepenalty = "Twoja drużyna otrzymała karę czasową za przyjazny ogień."
+lang.raiderkilledpenalty = "Twoja drużyna otrzymała karę czasową za utratę członka."
+lang.startingraid = "Rozpoczynając nalot, naciśnij E na skarbcu, aby anulować."
+lang.joinedraidparty = "Dołączyłeś do grupy rajdowej, naciśnij E na skarbcu, aby wyjść."
+lang.leftraid = "Opuściłeś grupę rajdową."
+lang.cancelledraid = "Anulowałeś nalot."
+lang.cantjoinleavestage = "W tej chwili nie możesz dołączyć do rajdu ani go opuścić."
+lang.cantrejoin = "Nie możesz ponownie dołączyć do tego nalotu."
+lang.warmupover = "Okres rozgrzewki dobiegł końca, broń skarbca, aby go otworzyć."
+lang.vaultbreached = "Naruszono zabezpieczenia skarbca, zbadaj to."
+lang.nonearbyvaults = "W pobliżu nie ma żadnych usterek, które są gotowe do zhakowania."
+lang.securitydisabled = "BEZPIECZEŃSTWO WYŁĄCZONE"
+lang.destroyingevidence = "ZNISZCZENIE DOWODU W %s"
+lang.standback = "PROSZĘ COFNIJ SIÉ"
+lang.hackerstop = "ZATRZYMAĆ"
+lang.hackerinstructions = [[BEZPIECZEŃSTWO SMASHER v2.0 INSTRUKCJE
+-------------------------------------------------
+1: Umieść to urządzenie w pobliżu skarbca bankowego.
+2: Uruchom urządzenie i naciśnij start.
+3: Ukończ proces hakowania, znajdując właściwe słowo w mieszaczu.
+4: Zutylizuj urządzenie w bezpieczny sposób.]]
+lang.securityalert = "ALARM BEZPIECZEŃSTWA"
+lang.counterraid = "LICZNIK"
+lang.leaveraid = "POZOSTAWIAĆ"
+lang.raidcountertitle = "!ALERT!"
+lang.raidcountermessage = [[Ochrona skarbca ma
+zostałeś naruszony, prawda? 
+lubisz przeciwdziałać nalotowi?]]
+lang.raidprogressmessage = [[RAIDERS LEFT: #l
+POLICE JOINED: #j
+POZOSTAŁY CZAS: #t]]
+lang.waypoint = "VAULT RAID W TOKU"
+
+return lang
