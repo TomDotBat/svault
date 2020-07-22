51855bace5eaabe6b6b597e63e1c788702819cd6
Tom <tom@tomdotbat.dev>
2020-07-22 17:35:53 +0100

    French translation

---
+++ lua/svault/lang/french.lua
@@ -0,0 +1,76 @@
+local lang = {}
+ 
+lang.securitysystem = "SYSTÈME DE SÉCURITÉ"
+lang.enabled = "ACTIVÉ"
+lang.disabled = "DÉSACTIVÉ"
+lang.alertcps = "MOUVEMENT DÉTECTÉ\nALERTE ENVOYER AUX FORCES DE L'ORDRE\nDANS:"
+lang.securityenabled = "DÉTECTEURS DE MOUVEMENT : ACTIVÉ\nCAMÉRAS DE SÉCURITÉ : EN DIRECT\nSYSTÈME DE SÉCURITÉ FONCTIONNEL"
+lang.securitydisabledmsg = "ATTENTION!\nSYSTÈME DE SÉCURITÉ EN PANNE\nVEUILLEZ TROUVER LE PROBLÈME"
+lang.robberystatus = "STATUT DU BRAQUAGE"
+lang.warmupperiod = "PÉRIODE D'ÉCHAUFFEMENT"
+lang.cooldownperiod = "EN ATTENTE"
+lang.vaultstatus = "STATUT DU COFFRE"
+lang.idle = "EN ATTENTE"
+lang.recovering = "RÉCUPÉRATION"
+lang.info = "INFO"
+lang.cooldown = "COOLDOWN"
+lang.active = "ACTIF"
+lang.inactive = "INACTIF"
+lang.players = "JOUEURS"
+lang.totalvalue = "VALEUR TOTALE"
+lang.lawenforcement = "FORCES DE L'ORDRE"
+lang.currentrobbers = "VOLEURS ACTUELS"
+lang.notapplicable = "N/A"
+lang.savevaultscmd = "/savevaults"
+lang.clearvaultscmd = "/clearvaults"
+lang.raidsuccess = "Le braquage a été couronné de succès, vous avez été récompensé par de l'argent provenant du coffre-fort de la banque."
+lang.nopermission = "Vous n'avez pas la permission de modifier l'emplacement des coffre-forts."
+lang.savedvaults = "Sauvegarde des emplacements de tous les coffres fort sur la carte."
+lang.removedvaults = "Supression de touts les coffres-forts."
+lang.cantstartcooldown = "Vous ne pouvez pas lancer un raid pendant une période d'attente"
+lang.wrongteam = "Vous n'êtes pas autorisé à faire des braquages dans le cadre de votre travail."
+lang.toomanyraids = "Il y a trop de braquages en cours pour en lancer un nouveau."
+lang.notenoughplayers = "Il n'y a pas assez de joueurs en ligne pour lancer un braquage."
+lang.notenoughpolice = "Il n'y a pas assez de policiers en ligne pour lancer un braquage."
+lang.alreadytakingpart = "Vous participez déjà à un braquage en cours."
+lang.alreadybeingraided = "Cette chambre forte fait déjà l'objet d'un braquage."
+lang.toomanyraiders = "Vous ne pouvez pas vous joindre à l'équipe de braquage pour ce coffre fort car elle est pleine."
+lang.toolate = "Vous ne pouvez plus vous joindre au braquage car il est trop tard."
+lang.cantchangeteam = "Vous ne pouvez pas changer de travail pendant un braquage."
+lang.cantsleep = "Vous ne pouvez pas dormir pendant un braquage."
+lang.cantstartvote = "Vous ne pouvez pas lancer un vote pendant un braquage."
+lang.killedraiderreward = "Vous avez été récompensé de %m pour avoir tué un braqueur "
+lang.friendlyfirepenalty = "Votre parti a reçu une pénalité de temps en raison d'un tir ami."
+lang.raiderkilledpenalty = "Votre parti a reçu une pénalité en temps pour avoir perdu un membre."
+lang.startingraid = "Démarrage d'un braquage, appuyez sur la touche E sur le coffre-fort pour annuler."
+lang.joinedraidparty = "Vous avez rejoint le groupe du braquage, appuyez sur la touche E du coffre-fort pour partir."
+lang.leftraid = "Vous avez quitté l'équipe du braquage."
+lang.cancelledraid = "Vous avez annulé le braquage."
+lang.cantjoinleavestage = "Vous ne pouvez pas rejoindre ou quitter le braquage à partir de maintenant."
+lang.cantrejoin = "Vous n'êtes pas autorisé à rejoindre ce braquage."
+lang.warmupover = "La période d'échauffement est terminée, défendez le coffre-fort pour l'ouvrir."
+lang.vaultbreached = "La sécurité d'une chambre forte a été violée, veuillez enquêter."
+lang.nonearbyvaults = "Il n'y a pas de chambres fortes à proximité qui soient prêtes à être piratées."
+lang.securitydisabled = "SÉCURITÉ DÉSACTIVÉE"
+lang.destroyingevidence = "DESTRUCTION DES PREUVES DANS %s"
+lang.standback = "VEUILLEZ RECULER"
+lang.hackerstop = "STOP"
+lang.hackerinstructions = [[SECURITY SMASHER v2.0 INSTRUCTIONS
+-------------------------------------------------
+1: Placez cet appareil près d'un coffre fort.
+2: Démarrez l'appareil et appuyez sur Start.
+3: Terminez le processus de piratage en trouvant le mot correct dans le brouilleur.
+4: Débarrasser vous de l'appareil en toute sécurité.]]
+lang.securityalert = "ALERTE DE SÉCURITÉ"
+lang.counterraid = "CONTRER"
+lang.leaveraid = "QUITTER"
+lang.raidcountertitle = "!ALERT!"
+lang.raidcountermessage = [[La sécurité d'un coffre-fort a
+a été violée, voulez-vous
+contrer le raid ?]]
+lang.raidprogressmessage = [[BRAQUEUR RESTANT #l
+POLICE REUNIE: #j
+TEMPS RESTANT: #t]]
+lang.waypoint = "BRAQUAGE D'UN COFFRES-FORTS EN COURS"
+ 
+return lang
