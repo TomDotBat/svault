4bf8b581a7d07a7a4c0b78d1fc2dac4be426ca5e
Tom <tom@tomdotbat.dev>
2020-07-22 17:37:43 +0100

    Turkish translation

---
+++ lua/svault/lang/turkish.lua
@@ -0,0 +1,74 @@
+local lang = {}
+
+lang.securitysystem = "GÜVENLİK SİSTEMİ"
+lang.enabled = "AKTİF"
+lang.disabled = "DEAKTİF"
+lang.alertcps = "HARAKET TESPİT EDİLDİ\nTÜM EMNİYET BİRİMLERİNE UYARI GİDİYOR\nIN:"
+lang.securityenabled = "HARAKET DEDEKTÖRLERİ: AKTİF\nGÜVENLİK KAMERALARI: AÇIK\nGÜVENLİK SİSTEMİ FONKSİYONEL"
+lang.securitydisabledmsg = "UYARI!\nGÜVENLİK SİSTEMİ ÇÖKTÜ\nLÜTFEN BİR GÖZ ATIN"
+lang.robberystatus = "SOYGUN DURUMU"
+lang.warmupperiod = "ISINMA PERİYODU"
+lang.cooldownperiod = "SOĞUMA PERIYODU"
+lang.vaultstatus = "KASA DURUMU"
+lang.idle = "BOŞTA"
+lang.recovering = "DÜZELTİLİYOR"
+lang.info = "BİLGİ"
+lang.cooldown = "COOLDOWN"
+lang.active = "AKTİF"
+lang.inactive = "İNAKTİF"
+lang.players = "OYUNCULAR"
+lang.totalvalue = "TOPLAM DEĞER"
+lang.lawenforcement = "EMNİYET GÜÇLERİ"
+lang.currentrobbers = "SOYGUNCULAR"
+lang.notapplicable = "N/A"
+lang.savevaultscmd = "/savevaults"
+lang.clearvaultscmd = "/clearvaults"
+lang.raidsuccess = "Baskın başarılı oldu, banka kasasından para aldın."
+lang.nopermission = "Banka kasalarının konumunu ayarlamaya yetkin yok."
+lang.savedvaults = "Tüm banka kasaları kaydedildi."
+lang.removedvaults = "Kayıtlar silindi ve tüm kasalar kaldırıldı."
+lang.cantstartcooldown = "Soğuma süresinde birkez daha soygun yapamazsın."
+lang.wrongteam = "Bu meslekteyken soygun yapamazsın."
+lang.toomanyraids = "Şu an bir çok yer soyuluyor. Lütfen bekleyiniz."
+lang.notenoughplayers = "Bir soygun yapmak için yeterli oyuncuya sahip değilsin."
+lang.notenoughpolice = "Yeterli emniyet güçleri bulunmadığından soygun yapamazsın."
+lang.alreadytakingpart = "Zaten bir soygunda bulunuyorsun."
+lang.alreadybeingraided = "Bu kasa zaten soyuluyor."
+lang.toomanyraiders = "Bu soygun partisine dolu olduğundan katılamazsın."
+lang.toolate = "Çok geç olduğundan bu soyguna katılamazsın."
+lang.cantchangeteam = "Soygun sırasında meslek değiştiremezsin."
+lang.cantsleep = "Baskın sırasında uyuyamazsın."
+lang.cantstartvote = "Soygun sırasında oylama başaltamazsın."
+lang.killedraiderreward = "Bir soyguncuyu öldürdüğünden %m kazandın."
+lang.friendlyfirepenalty = "Dost ateşi yaptığından dolayı zaman cezası aldın."
+lang.raiderkilledpenalty = "Bir üyeyi kaybettiğinizden dolayı ."
+lang.startingraid = "Bir soygun başlıyor, kasaya E tıklayarak iptal et."
+lang.joinedraidparty = "Soygun partisine katıldın, kasaya e tıklayarak iptal et."
+lang.leftraid = "Soygun partisinden ayrıldın."
+lang.cancelledraid = "Soygunu iptal ettin."
+lang.cantjoinleavestage = "Şu andan itibaren soygundan ayrılamazsın."
+lang.cantrejoin = "Soyguna yeniden katılamazsın."
+lang.warmupover = "Isınma periyodu bitti, kasayı savunarak açılmasını sağla."
+lang.vaultbreached = "Kasanın güvenliği aşıldı, lütfen araştırın."
+lang.nonearbyvaults = "Hacklenicek yakında bir kasa yok."
+lang.securitydisabled = "GÜVENLİK DEAKTİF EDİLDİ"
+lang.destroyingevidence = "KANITLAR %s İÇİNDE SİLİNİYOR"
+lang.standback = "LUTFEN ARKADA DURUN"
+lang.hackerstop = "DUR"
+lang.hackerinstructions = [[GÜVENLİK EZİCİ v2.0 TALİMATLARI
+-------------------------------------------------
+1: Bir banka aksasının yanına koy.
+2: Aygıtı hazırla ve başla tuşuna tıkla.
+3: Kelimelikte doğru kelimeyi seçerek hileyi tamamla.
+4: Cihazı güvenli bir şekilde elden çıkarın.]]
+lang.securityalert = "GÜVENLİK ALARMI"
+lang.counterraid = "SAYAÇ"
+lang.leaveraid = "AYRIL"
+lang.raidcountertitle = "!UYARI!"
+lang.raidcountermessage = [[Bir kasanın güvenliği aşıldı. Karşı koymak ister misin?]]
+lang.raidprogressmessage = [[KALAN SOYGUNCULAR: #l
+KATILAN POLISLER: #j
+KALAN SURE: #t]]
+lang.waypoint = "SOYGUN DEVAM EDİYOR"
+
+return lang
